b8543e5c6b12b759e16f9f0b959f4d22c44a591e
Rashmi Singh <112812334+rashmigitrepo@users.noreply.github.com>
2023-08-27 02:42:03 +0530

    Update my_custom_module.py

--- my_custom_module.py
+++ my_custom_module.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 from ansible.module_utils.basic import AnsibleModule
-
 metadata = {
     'name': 'my_custom_module',
     'argument_spec': dict(
@@ -8,7 +7,6 @@ metadata = {
         another_argument=dict(type='int', required=False, default=5)
     )
 }
-
 def main():
     module = AnsibleModule(
         argument_spec=metadata['argument_spec'],
@@ -17,16 +15,12 @@ def main():
     
     my_argument_value = module.params['my_argument']
     another_argument_value = module.params['another_argument']
-    
     # Your module logic here
     result_message = f"Received '{my_argument_value}'. Custom message: Argument value plus {another_argument_value}"
-    
     result = {
         'changed': True,
         'result_message': result_message,
     }
-    
     module.exit_json(**result)
-
 if __name__ == '__main__':
     main()
